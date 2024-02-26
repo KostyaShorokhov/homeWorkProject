49995b3f37de97ad6d149d5c085ad1b3e21c32f5
k.shorokhov <k.shorokhov@pyn.ru>
2024-02-26 10:38:11 +0700

    task 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,72 @@ package org.example;
 public class Main {
 
   public static void main(String[] args) {
+    getResume(
+        "Иванов",
+        "Кирилл",
+        "Иванович"
+    );
+    getResume(
+        "Сидоров",
+        "Егор",
+        "Михайлович",
+        31,
+        "Прораб"
+    );
+    getResume(
+        "Петров",
+        "Никита",
+        "Олегович",
+        26,
+        "Механик",
+        1.5f
+    );
+  }
+
+  private static void getResume(
+      String lastName,
+      String firstName,
+      String middleName
+  ) {
+    System.out.println(
+        "Фамилия: " + lastName + ", " +
+            "Имя: " + firstName + ", " +
+            "Отчество: " + middleName
+    );
+  }
+
+  private static void getResume(
+      String lastName,
+      String firstName,
+      String middleName,
+      int age,
+      String profession
+  ) {
+    System.out.println(
+        "Фамилия: " + lastName + ", " +
+            "Имя: " + firstName + ", " +
+            "Отчество: " + middleName + ", " +
+            "Возраст: " + age + ", " +
+            "Профессия: " + profession
+    );
+  }
+
+  private static void getResume(
+      String lastName,
+      String firstName,
+      String middleName,
+      int age,
+      String profession,
+      float experience
+  ) {
+    System.out.println(
+        "Фамилия: " + lastName + ", " +
+            "Имя: " + firstName + ", " +
+            "Отчество: " + middleName + ", " +
+            "Возраст: " + age + ", " +
+            "Профессия: " + profession + ", " +
+            "Опыт: " + experience
+    );
   }
 }
+
